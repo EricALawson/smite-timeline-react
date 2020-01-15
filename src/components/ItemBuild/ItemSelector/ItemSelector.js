9bcf6cc356af9bac019d444562aa139485633c79
EatThisShoe <eric.alexander.lawson@gmail.com>
2020-01-14 19:04:32 -0800

    fix to previous commit. It wouldn't compile with unused import

--- src/components/ItemBuild/ItemSelector/ItemSelector.js
+++ src/components/ItemBuild/ItemSelector/ItemSelector.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import './ItemSelector.css';
 import '../../../hooks/useOutsideClick';
-import useOutsideClick from '../../../hooks/useOutsideClick';
 
 const ItemSelector = ({openState, closeSelector}) => {
     // const ref = React.useRef();
